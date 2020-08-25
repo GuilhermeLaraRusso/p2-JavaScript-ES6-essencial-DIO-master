6f1b4ce7479138da0ebc5af057c34d6b1a35fd19
Guilherme <guilhermelrusso@gmail.com>
2020-08-25 15:03:49 -0300

    adicionando comentários no ex-2-slice.js

--- Modulo4/aula1/ex-2-slice.js
+++ Modulo4/aula1/ex-2-slice.js
@@ -1,10 +1,14 @@
 
 const arr = [1, 2, 3, 4, 5];
 
+// pega a posição inicial até antes da posição do segundo fator
 console.log(arr.slice(0,2));
 
+// pega a partir da segunda posição, incluindo a posição 2
 console.log(arr.slice(2));
 
+// pega a pultima posição
 console.log(arr.slice(-1));
 
+// pega a partir de 3 posições para a última até a última
 console.log(arr.slice(-3));
